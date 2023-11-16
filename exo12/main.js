4ce12fa2405e01be7124a38e39a7e9b49d8e9ad1
oceaneheb <oceane47.heb@gmail.com>
2023-11-16 16:57:34 +0100

    Suppression des fichiers

--- exo12/main.js
+++
@@ -1,5 +0,0 @@
-let nombreDepart = parseInt(prompt("Quel est votre nombre ?"));
-for (let i=1; i<11; i++) {
-    let multiplication = nombreDepart * i;
-    console.log(`${nombreDepart} x ${i} = ${multiplication}`)
-}
--- exo3/main.js
+++
@@ -1,8 +0,0 @@
-let nombre1 = parseInt(prompt("Entrez votre premier nombre : "));
-let nombre2 = parseInt(prompt("Entrez votre deuxième nombre :"));
-
-if ((nombre1 >0 && nombre2 >= 0) || (nombre1 <0 && nombre2 <0)) {
-    console.log("C'est positif");
-} else {
-    console.log("C'est négatif")
-}
